d45bb49fd788b387275adfe4189d2adc0ff9f4f7
Khabo Morolong <khabomorolong@gmail.com>
2024-07-14 08:32:43 +0200

    Added rights for POD and Dispensing

---
+++ src/main/resources/db/migration/20240713221730515__add_rights_for_pod_dispensing.sql
@@ -0,0 +1,7 @@
+INSERT INTO rights (id, description, name, type) VALUES ('b13ef91b-6051-487b-b0a3-df471abad8ee', NULL, 'POD_RECEIVE', 'SUPERVISION');
+INSERT INTO rights (id, description, name, type) VALUES ('5f9901fe-f947-4c2d-8b79-80203e6de552', NULL, 'POD_VIEW', 'SUPERVISION');
+INSERT INTO rights (id, description, name, type) VALUES ('7169da19-ae22-46f3-9df0-7b8205987185', NULL, 'PREPACKING_CREATE', 'SUPERVISION');
+INSERT INTO rights (id, description, name, type) VALUES ('7ad7143d-db3f-416e-acb2-c7ce1124e423', NULL, 'PREPACKING_AUTHORIZE', 'SUPERVISION');
+INSERT INTO rights (id, description, name, type) VALUES ('8d231bd4-7634-47e6-8337-519c2b09edba', NULL, 'PREPACKING_VIEW', 'SUPERVISION');
+INSERT INTO rights (id, description, name, type) VALUES ('26b70109-cc44-425d-bc94-c5077cf84a50', NULL, 'DISPENSING', 'SUPERVISION');
+

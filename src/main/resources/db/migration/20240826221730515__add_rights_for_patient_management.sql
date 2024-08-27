216f651f665e57f9dd380fe8d47aa76185b62958
Khabo Morolong <khabomorolong@gmail.com>
2024-08-27 05:02:41 +0200

    Added rights for patient management

---
+++ src/main/resources/db/migration/20240826221730515__add_rights_for_patient_management.sql
@@ -0,0 +1,5 @@
+INSERT INTO rights (id, description, name, type) VALUES ('08a3b0ef-b487-4a39-a320-86e01c5f8968', NULL, 'PATIENT_CREATE', 'SUPERVISION');
+INSERT INTO rights (id, description, name, type) VALUES ('a7a7d8d8-ba3d-4cec-b115-799488aa601c', NULL, 'PATIENT_UPDATE', 'SUPERVISION');
+INSERT INTO rights (id, description, name, type) VALUES ('feb8ff10-7553-45ce-9257-4c915a6594d5', NULL, 'PATIENT_VIEW', 'SUPERVISION');
+INSERT INTO rights (id, description, name, type) VALUES ('ffd453e0-dc1f-468f-a7dc-f516cd84abb6', NULL, 'PATIENT_REMOVE', 'SUPERVISION');
+
